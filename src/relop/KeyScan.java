8c49565a8040e4d51c6df771741cdb7b2012354c
Elliott Mantock <emantock@gmail.com>
2016-03-08 16:12:18 -0500

    implement isOpen and close

--- src/relop/KeyScan.java
+++ src/relop/KeyScan.java
@@ -13,10 +13,13 @@ public class KeyScan extends Iterator {
   private HeapFile file;
   private HashScan scan;
 
+  private boolean isopen;
+
   /**
    * Constructs an index scan, given the hash index and schema.
    */
   public KeyScan(Schema schema, HashIndex index, SearchKey key, HeapFile file) {
+    this.isopen = true;
     this.setSchema(schema);
     this.scan = index.openScan(key);
     this.file = file;
@@ -27,6 +30,7 @@ public class KeyScan extends Iterator {
    * child iterators, and increases the indent depth along the way.
    */
   public void explain(int depth) {
+    // TODO Not really sure what its asking.
     throw new UnsupportedOperationException("Not implemented");
   }
 
@@ -34,6 +38,7 @@ public class KeyScan extends Iterator {
    * Restarts the iterator, i.e. as if it were just constructed.
    */
   public void restart() {
+    // TODO Not sure about the best way to do this yet.
     throw new UnsupportedOperationException("Not implemented");
   }
 
@@ -41,14 +46,15 @@ public class KeyScan extends Iterator {
    * Returns true if the iterator is open; false otherwise.
    */
   public boolean isOpen() {
-    throw new UnsupportedOperationException("Not implemented");
+    return isopen;
   }
 
   /**
    * Closes the iterator, releasing any resources (i.e. pinned pages).
    */
   public void close() {
-    throw new UnsupportedOperationException("Not implemented");
+    scan.close();
+    isopen = false;
   }
 
   /**
