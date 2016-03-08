7640d2e49b4bdd0c58d15156f0a4651173b97a21
Elliott Mantock <emantock@gmail.com>
2016-03-08 11:30:11 -0500

    added method stubs

--- src/relop/HashJoin.java
+++ src/relop/HashJoin.java
@@ -2,5 +2,40 @@ package relop;
 
 public class HashJoin extends Iterator {
 
-	
+  @Override
+  public void restart() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public Tuple getNext() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void explain(int depth) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void close() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean isOpen() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean hasNext() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
 }
