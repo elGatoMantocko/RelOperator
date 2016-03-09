c1113b7c8f4e26f3c79d6e3a1a1a83002a4c2224
Elliott Mantock <emantock@gmail.com>
2016-03-09 14:08:03 -0500

    reverted the previous commit

--- src/relop/Selection.java
+++ src/relop/Selection.java
@@ -16,7 +16,6 @@ public class Selection extends Iterator {
    * Constructs a selection, given the underlying iterator and predicates.
    */
   public Selection(Iterator iter, Predicate... preds) {
-    super(iter);
     this.scan = iter;
     this.preds = preds;
 
