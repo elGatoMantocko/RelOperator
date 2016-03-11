bf9966953d0bfe563b05dc66d66fb67af8e7c217
David Tschida <dmtschida1@gmail.com>
2016-03-11 17:40:37 -0500

    Read all outer into hashmap.

--- src/main/java/relop/HashJoin.java
+++ src/main/java/relop/HashJoin.java
@@ -1,7 +1,5 @@
 package relop;
 
-import global.PageId;
-
 import java.util.Map;
 
 public class HashJoin extends Iterator {
@@ -9,53 +7,53 @@ public class HashJoin extends Iterator {
     private Map<Object, Tuple> mMap;
 
     public HashJoin(FileScan outer, FileScan inner, int outercolnum, int innercolnum) {
-    // TODO HashJoin constructor with two filescans
-    // not sure how to initialize this yet
-    mMap = new MultipleValueTreeMap();
-    while(outer.hasNext()) {
-        Tuple next = outer.getNext();
+        // TODO HashJoin constructor with two filescans
+        // not sure how to initialize this yet
+        mMap = new MultipleValueTreeMap<Object, Tuple>();
+        while (outer.hasNext()) {
+            Tuple next = outer.getNext();
+            mMap.put(next.getField(outercolnum), next);
+        }
+    }
+
+    public HashJoin(HashJoin hj, IndexScan scan, int outercolnum, int innercolnum) {
+        // TODO HashJoin copy constructor
+        // not sure how to initialize this yet
+    }
+
+    @Override
+    public void restart() {
+        // TODO Auto-generated method stub
 
     }
-  }
 
-  public HashJoin(HashJoin hj, IndexScan scan, int outercolnum, int innercolnum) {
-    // TODO HashJoin copy constructor
-    // not sure how to initialize this yet
-  }
+    @Override
+    public void explain(int depth) {
+        // TODO Auto-generated method stub
 
-  @Override
-  public void restart() {
-    // TODO Auto-generated method stub
+    }
 
-  }
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
 
-  @Override
-  public void explain(int depth) {
-    // TODO Auto-generated method stub
+    }
 
-  }
+    @Override
+    public boolean isOpen() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-  @Override
-  public void close() {
-    // TODO Auto-generated method stub
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-  }
-
-  @Override
-  public boolean isOpen() {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public boolean hasNext() {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public Tuple getNext() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+    @Override
+    public Tuple getNext() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
